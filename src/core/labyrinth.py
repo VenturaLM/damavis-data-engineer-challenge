c2f41a32b6da62cea6ad995f07c461d15ca86fc3
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2023-08-24 16:47:11 +0200

    Checks, read in json format, valid movement fixed and valid rotation within A*.

--- src/core/labyrinth.py
+++ src/core/labyrinth.py
@@ -1,5 +1,6 @@
+import json
+
 from core.node import Node
-from core.rod import Rod
 
 
 class Labyrinth_solver:
@@ -18,8 +19,10 @@ class Labyrinth_solver:
         self.labyrinth = None
         # Labyrinth shape.
         self.labyrinth_shape = None
-        # Rod instance.
-        self.rod = Rod()
+        # Starting point.
+        self.start = None
+        # Ending point.
+        self.end = None
         # The rod can only be rotated about its center, and only if the 3 x 3 area surrounding it is
         # clear from the obstacles or the walls.
         self.obstacle_constraint = (3, 3)
@@ -28,6 +31,67 @@ class Labyrinth_solver:
 
         # Read labyrinth from file.
         self._read_labyrinth(path=path)
+
+    def _check_labyrinth(self) -> None:
+        """ 
+        ### Description
+        ---------------
+        Method that checks:
+        1. The labyrinth is a `list` instance.
+        2. The number of rows is in [3, 1000] range.
+        3. The number of columns is in [3, 1000] range.
+
+        ### Raises
+        ----------
+        - `TypeError`: if `self.labyrinth` is not a `list` instance.
+        - `ValueError`: either number of rows or columns is not in range.
+        """
+        # Check if `labyrinth` belongs to a valid class.
+        if not isinstance(self.labyrinth, list):
+            raise TypeError(
+                f"[Labyrinth_solver] Inappropriate argument type in 'labyrinth' while initializing labyrinth: {type(self.labyrinth)}."
+            )
+
+        self.labyrinth_shape = (len(self.labyrinth) - 1, len(self.labyrinth[0]) - 1)
+
+        if not 2 <= self.labyrinth_shape[0] <= 999:
+            raise ValueError(
+                f"[Labyrinth_solver] The number of rows of the labyrinth must be [3, 1000] (Current {self.labyrinth_shape[0] + 1})."
+            )
+        if not 2 <= self.labyrinth_shape[1] <= 999:
+            raise ValueError(
+                f"[Labyrinth_solver] The number of columns of the labyrinth must be [3, 1000] (Current {self.labyrinth_shape[1] + 1})."
+            )
+
+    def _check_start(self) -> None:
+        """
+        ### Description
+        ---------------
+        Method that checks that the starting point type is a tuple instance.
+
+        ### Raises
+        ----------
+        - `TypeError`: if `self.start` is not a `tuple` instance.
+        """
+        if not isinstance(self.start, tuple):
+            raise TypeError(
+                f"[Labyrinth_solver] Inappropriate argument type in 'start' while initializing labyrinth: {type(self.start)}."
+            )
+
+    def _check_end(self) -> None:
+        """
+        ### Description
+        ---------------
+        Method that checks that the ending point type is a tuple instance.
+
+        ### Raises
+        ----------
+        - `TypeError`: if `self.end` is not a `tuple` instance.
+        """
+        if not isinstance(self.end, tuple):
+            raise TypeError(
+                f"[Labyrinth_solver] Inappropriate argument type in 'end' while initializing labyrinth: {type(self.end)}."
+            )
 
     def _read_labyrinth(self, path: str) -> None:
         """
@@ -41,37 +105,48 @@ class Labyrinth_solver:
         """
         # Read file content.
         with open(path, "r") as file:
-            content = file.read()
+            content = json.load(file)
 
-        # Evaluate expression of `content` as python code.
-        labyrinth = eval(content)
+        try:
+            self.labyrinth = eval(content["labyrinth"])
+            self._check_labyrinth()
+        except Exception as e:
+            raise e
 
-        # Check if `labyrinth` belong to a valid class.
-        if isinstance(labyrinth, list):
-            self.labyrinth = eval(content)
-            self.labyrinth_shape = (len(self.labyrinth) - 1, len(self.labyrinth[0]) - 1)
-        else:
-            raise TypeError(
-                f"[Labyrinth_solver] Inappropriate argument type while initializing labyrinth: {type(labyrinth)}."
-            )
+        self.start = eval(content["start"])
+        self._check_start()
 
-    def is_valid_move(self, x: int, y: int) -> bool:
+        self.end = eval(content["end"])
+        self._check_end()
+
+    def is_within_labyrinth(self, x: int, y: int) -> bool:
         """
         ### Description
         ---------------
-        Checks wether a move is valid within the labyrinth or not.
+        Checks wether a move is within the labyrinth or not.
 
         ### Parameters
         --------------
         - `x`: coordinate x.
         - `y`: coordinate y.
         """
-        if (
-            0 <= x <= self.labyrinth_shape[0]
-            and 0 <= y <= self.labyrinth_shape[1]
-            and self.labyrinth[x][y] == "."
-        ):
+        if 0 <= x <= self.labyrinth_shape[0] and 0 <= y <= self.labyrinth_shape[1]  and self.labyrinth[x][y] == ".":
             return True
+
+        return False
+    
+    def is_valid_move(self, node: Node, x: int, y: int) -> bool:
+        """
+
+        """        
+        if node.orientation == "horizontal":
+            if y - 1 >= 0 and y + 1 <= self.labyrinth_shape[1]:
+                if self.labyrinth[x][y - 1] == "." and self.labyrinth[x][y + 1] == ".":
+                    return True
+        if node.orientation == "vertical":
+            if x - 1 >= 0 and x + 1 <= self.labyrinth_shape[0]:
+                if self.labyrinth[x - 1][y] == "." and self.labyrinth[x + 1][y] == ".":
+                    return True
 
         return False
 
@@ -124,27 +199,22 @@ class Labyrinth_solver:
                         return False
         return True
 
-    def a_star(self, start: set, end: set):
+    def a_star(self):
         """
         ### Description
         ---------------
         Method that solves the labyrinth using A* Path-finding algorithm.
-
-        ### Parameters
-        --------------
-        - `start`: starting point.
-        - `end`: ending point.
 
         ### Returns
         -----------
         List of tuples as a path from the given start to the given end in the given labyrinth.
         """
         # Start node.
-        start_node = Node(parent=None, position=start)
+        start_node = Node(parent=None, position=self.start)
         start_node.g = start_node.h = start_node.f = 0
 
         # End node.
-        end_node = Node(parent=None, position=end)
+        end_node = Node(parent=None, position=self.end)
         end_node.g = end_node.h = end_node.f = 0
 
         # Initialize both open and closed list.
@@ -163,45 +233,52 @@ class Labyrinth_solver:
                 if item.f < current_node.f:
                     current_node = item
                     current_index = index
-                    self.rod.position = current_node.position
+
+                    # Check rod constraints.
+                    if current_node.orientation == "horizontal":
+                        if self.is_valid_rotation(
+                            x=current_node.position[0], 
+                            y=current_node.position[1], 
+                            orientation="vertical"
+                        ):
+                            current_node.orientation = "vertical"
+                    else:
+                        if self.is_valid_rotation(
+                            x=current_node.position[0], 
+                            y=current_node.position[1], 
+                            orientation="horizontal"
+                        ):
+                            current_node.orientation = "horizontal"
+
+                current_node.compute_rod_position()
 
             # Pop current off open list, add to closed list.
             open_list.pop(current_index)
             closed_list.append(current_node)
 
             # Found the goal.
-            if current_node == end_node:
+            if end_node.position in current_node.rod_position:
                 path = []
                 current = current_node
                 while current is not None:
-                    path.append(current.position)
+                    path.append((current.position, current.orientation))
                     current = current.parent
                 return path[::-1]  # Return reversed path.
 
             # Generate children.
             children = []
-            for dx, dy in self.rod.movements:
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]: # Movements.
                 # Get node position.
                 node_position = (
                     current_node.position[0] + dx,
                     current_node.position[1] + dy,
                 )
 
-                # ==================================================================================
                 # Checks.
-                if self.is_valid_move(x=node_position[0], y=node_position[1]) == False:
+                if self.is_within_labyrinth(x=node_position[0], y=node_position[1]) == False:
                     continue
-
-                # if (
-                #     self.is_valid_rotation(
-                #         x=node_position[0],
-                #         y=node_position[1],
-                #         orientation=self.rod.orientation,
-                #     )
-                #     == False
-                # ):
-                #     continue
-                # ==================================================================================
+                if self.is_valid_move(node=current_node, x=node_position[0], y=node_position[1]) == False:
+                    continue
 
                 # Create new node.
                 new_node = Node(current_node, node_position)
@@ -234,20 +311,6 @@ class Labyrinth_solver:
 
                 # Add the child to the open list.
                 open_list.append(child)
-                ...
-
-    def print_solution(self):
-        """
-        ### Description
-        ---------------
-        Prints the solution to the labyrinth problem passed as argument firstly with the following
-        structure:
-
-        >>> solution = {'path': [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 5), (1, 6), (2, 6), (2, 7), (2, 8), (3, 8), (4, 8)], 'path_length': 13}
-        """
-        print(f"Solution:")
-        print(f"=========")
-        print(f"{self.solution}")
 
     def solve(self) -> int:
         """
@@ -255,8 +318,8 @@ class Labyrinth_solver:
         ---------------
         Core method to solve the labyrinth.
         """
-        path = self.a_star(start=(0, 0), end=(4, 8))
-        path_length = len(path)
+        path = self.a_star()
+        path_length = -1 if path is None else len(path)
 
         self.solution["path"] = path
-        self.solution["path_length"] = path_length
+        self.solution["path_length"] = path_length - 1 # Subtract the first move (0, 0).
