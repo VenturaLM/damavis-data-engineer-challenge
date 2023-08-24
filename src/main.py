34a57ba1689002c4ed42cb44382130cfb965468f
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2023-08-24 20:37:51 +0200

    Command line parser added.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import argparse
+
 from core.labyrinth import Labyrinth_solver
 
 
@@ -6,13 +8,26 @@ def main():
     ### Description
     ---------------
     Main script to run the Labyrinth solver.
-    """
 
-    # for i in range(1, 5):
-    # print(f"\nTest {i}:")
-    solver = Labyrinth_solver(path=f"test/test_{1}.txt")
-    solver.solve()
-    solver.print_solution()
+    ### Example
+    -----------
+    >>> python3 main.py -L ../test/test_3.json ../test/test_4.json ../test/test_2.json
+    """
+    # Command line argument parser.
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-L", "--labyrinth_path", nargs="+", type=str, help="Path of the labyrinth(s) data. It takes 1 or more paths", required=True
+    )
+
+    args = parser.parse_args()
+    
+    # Run each file in passed in `args.labyrinth_path`.
+    for file in args.labyrinth_path:
+        print(f"\nFile {file}:")
+        print("===========================")
+        solver = Labyrinth_solver(path=file)
+        solver.solve()
+        print(solver.solution)
 
 
 if __name__ == "__main__":
