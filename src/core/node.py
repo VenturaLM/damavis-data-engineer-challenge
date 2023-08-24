7d09512d907066214e9164f5a147144497096d80
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2023-08-24 16:47:46 +0200

    Rod class merged into Node one.

--- src/core/node.py
+++ src/core/node.py
@@ -1,11 +1,23 @@
 class Node:
     """
+    ### Description
+    ---------------
     A node class for A* Path-finding.
+
+    ### Parameters
+    --------------
+    - `parent`: parent node.
+    - `position`: position within the graph.
     """
 
     def __init__(self, parent=None, position=None):
+        # Parent node.
         self.parent = parent
+        # Position within the graph.
         self.position = position
+        # Rod projection.
+        self.orientation = "horizontal"
+        self.rod_position = self.compute_rod_position()
 
         # Cost of reaching next node.
         self.g = 0
@@ -14,5 +26,24 @@ class Node:
         # Cheapest cost.
         self.f = 0
 
-    def __eq__(self, other):
-        return self.position == other.position
+    def compute_rod_position(self):
+        """
+        ### Description
+        ---------------
+        Compute rod's position with the current `self.position`.
+
+        ### Warning
+        -----------
+        This method assumes that the rod's shape is 3x1 (horizontal) and 1x3 (vertical).
+        """
+        if self.orientation == "horizontal":
+            self.rod_position = [
+                (self.position[0], self.position[1] - 1),
+                (self.position[0], self.position[1] + 1),
+            ]
+
+        if self.orientation == "vertical":
+            self.rod_position = [
+                (self.position[0] - 1, self.position[1]),
+                (self.position[0] + 1, self.position[1]),
+            ]
